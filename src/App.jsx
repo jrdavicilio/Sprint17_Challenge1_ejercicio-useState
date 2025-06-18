e82b278aad0e4efb5ae5a4ad1cac363fadd12bdf
David Fernández <davidferal26@gmail.com>
2025-06-18 23:44:06 +0200

    PROYECTO FINAL

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
- //Aquí tu código
+  const [name, setName] = useState('Sofía');
+  const [newName, setNewName] = useState('');
+
+  const changeName = () => {
+
+    if (newName !== '') {
+      setName(newName);
+      setNewName('');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Teacher Name: {name}</h2>
+
+      <form onSubmit={changeName}>
+        <input
+          type="text"
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+          placeholder="add a name"
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
